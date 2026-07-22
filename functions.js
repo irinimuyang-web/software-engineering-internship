fb71c620784c854eafa41720acc0259d329c48c8
irinimuyang-web <irinimuyang@gmail.com>
2026-07-22 10:51:28 +0100

    add functions.js

---
+++ functions.js
@@ -0,0 +1,16 @@
+//function to checkmifa number is even
+function isEven(number){
+    return number % 2===0;
+}
+console.log(isEven(8));
+console.log(isEven(9));
+//function 2:count the number of characters in a sting
+function countCharacters(text){
+    return text.lenght;
+}
+console.log(countCharacters(Engineering));
+//function 3:find the smallest number
+function smallestNumber(a,b,c){
+    return Math.min(a,b,c);
+}
+console.log(smallestNumber(10,4,7));
